c921411e43845c5888240f8f83352452361db4d6
Proyecto-grupo9 <lccproyecto2606@gmail.com>
2026-06-15 20:10:28 +0000

    Solucion de error en la funcion main

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #Importamos el codigo principal
-import proyecto
+from proyecto import *
 
 
 def main():
-    #Resolucion pregunta 6
-    print(cantidades_ventas_segment(leer_archivo()))
-    #Resolucion pregunta 3
-    estado_x = input("Ingrese un estado de USA: \n")
-    print(cantidades_envio_estado(leer_archivo(), estado_x))
-
-
-
     return 0
-
-if __name__ == "__main__":
-    main()
