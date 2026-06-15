cd772b05f2e2684a055584169c0b4e3d00e681b9
Proyecto-grupo9 <lccproyecto2606@gmail.com>
2026-06-15 19:36:34 +0000

    Agregamos al main la resolucion de la pregunta 3

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-import cantidades_ventas_segment
+#Importamos el dataset
 import leer_archivo
 
+#Importamos los archivos de la resolucion de la pregunta 6
+import cantidades_ventas_segment
+
+#Importamos los archivos de la resolucion de la pregunta 3
+import resolucion_pregunta3
+
+
 def main()
+    #Resolucion pregunta 6
     print(cantidades_ventas_segment(leer_archivo()))
+    #Resolucion pregunta 3
+    estado_x = input("Ingrese un estado de USA: \n")
+    print(resolucion_pregunta3(leer_archivo(), estado_x))
+
+
+
     return 0
