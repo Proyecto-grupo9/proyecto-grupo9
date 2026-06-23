43848bbf9925d6d9984d70039ea1a4f98c077e3a
Natividad <natividadvicogimena@gmail.com>
2026-06-23 06:25:41 -0300

    Update resolucion_pregunta1.py

--- resolucion_pregunta1.py
+++ resolucion_pregunta1.py
@@ -1,7 +1,16 @@
 
 def ventas_ganancias(ciudad:str,data_base:dict)->tuple:
     '''
-    
+    ciudad: string
+    ciudad: representa una ciudad de Estados Unidos
+    La función toma una ciudad de USA y un database y devuelve una tupla con la 
+    cantidad de ventas y sus respectivas ganancias.
+    Esta función se diseñó para calcular las ventas destinadas a una ciudad
+    específica y las ganancias que generaron las mismas.
+    Ejemplos;
+    ventas_ganancias( ):
+    ventas_ganancias( ):
+    vebtas_ganabcias( ):
     '''
     contador_quantity : int = 0
     ganancias : int = 0
