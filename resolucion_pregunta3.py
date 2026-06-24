9b938ee955ff9c3daea2c5a6cbd1c6db89f03e1b
Natividad <natividadvicogimena@gmail.com>
2026-06-24 10:16:29 -0300

    Update resolucion_pregunta3.py

--- resolucion_pregunta3.py
+++ resolucion_pregunta3.py
@@ -1,4 +1,16 @@
 def contar_envios(database, estado):
+    '''
+    Estado: es un string que representa un Estado de USA.
+    La función recibe un database y un Estado y devuelve las cantidades de cada tipo de envío que se hayan
+    utilizado para transportar la mercadería al Estado seleccionado.
+    Esta función se diseñó para mostrar las cantidades de cada tipo de envío a cada Estado. Es decir,
+    muestra cuantos envíos se hicieron por Standar Class, cuantos por First Class, cuantos por Second
+    Class y cuantos por Same Day.
+    Ejemplos:
+    test_contar_envios( ):
+    test_contar_envios( ):
+    test_contar_envios( ):
+    '''
     cantidades = {'Standard Class': 0, 'First Class': 0, 'Second Class': 0, 'Same Day': 0}
 
     
